29629dfd55dce181bedaa7391515bf3dcda82b6c
Jordan Goldstein <mrjordangoldstein@gmail.com>
2012-12-08 18:42:56 -0500

    Add basic display window

--- lib/pathfinding.rb
+++ lib/pathfinding.rb
@@ -4,3 +4,8 @@ require "gosu"
 # Require internal files
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+
+require "pathfinding/display_window"
+
+display = DisplayWindow.new
+display.show
---
+++ lib/pathfinding/display_window.rb
@@ -0,0 +1,12 @@
+class DisplayWindow < Gosu::Window
+  def initialize
+    super(640, 640, false)
+    self.caption = "Pathfinding Algorithm Display"
+  end
+  
+  def update
+  end
+  
+  def draw
+  end
+end
